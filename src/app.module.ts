805e9052fd2d7f800fd5793de0302417bc3c76fd
m7mdhany <muhamedhany.m@gmail.com>
2025-10-20 23:22:45 +0300

    feat:mongo db api added

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ReposModule } from './modules/repos/repos.module';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot(
+      'mongodb+srv://saidmagdy511_db_user:WPUMAvGsTYfOIqS6@cluster0.dkcpsk.mongodb.net/Cash'
+    ),
+    ReposModule,
+
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
