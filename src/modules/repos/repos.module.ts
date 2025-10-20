9d64b251235d83029777babffaf7cebba656dba2
m7mdhany <muhamedhany.m@gmail.com>
2025-10-20 23:29:46 +0300

    feat:repos schema added

--- src/modules/repos/repos.module.ts
+++ src/modules/repos/repos.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { ReposService } from './repos.service';
 import { ReposController } from './repos.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Repos, ReposSchema } from './schemas/repos.schema';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: Repos.name, schema: ReposSchema }])
+  ],
   controllers: [ReposController],
   providers: [ReposService],
 })
-export class ReposModule {}
+export class ReposModule { }
